4eabfb34b4de161da2ac326cf6cec351a164f277
Mariano <marianosciacco@gmail.com>
2020-05-05 14:41:36 +0200

    Delete user

--- ums/src/app/user/user.component.ts
+++ ums/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from './../users/user.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   // inputs: ['user:user-data'], // Alternativa per @input
@@ -8,10 +9,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  @Input('user-data') user // user-data è un alias
-  constructor() { }
+  @Input('user-data') user; // user-data è un alias
+  @Output('onDeleteUser') userDeleted = new EventEmitter(); // Evento di cancellazione
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  deleteUser(){
+    this.userDeleted.emit(this.user); // Emissione Evento
+    // this.userService.deleteUsers(this.user);
   }
 
 }
--- ums/src/app/users/user.service.ts
+++ ums/src/app/users/user.service.ts
@@ -1,42 +1,51 @@
 import { Injectable } from '@angular/core';
 
-
 @Injectable()
 export class UserService {
+
+    users =  [
+        {
+            name: "Mario",
+            lastname: "Rossi",
+            email: "mariorossi@email.it",
+            fiscalcode: "F192393FF83493",
+            province: "PD",
+            phone: "13939404"
+        },
+        {
+            name: "Mario2",
+            lastname: "Rossi2",
+            email: "mariorossi2@email.it",
+            fiscalcode: "C192393FF83493",
+            province: "PD",
+            phone: "13939404"
+        },
+        {
+            name: "Mario3",
+            lastname: "Rossi3",
+            email: "mariorossi3@email.it",
+            fiscalcode: "A192393FF83493",
+            province: "VE",
+            phone: "13939404"
+        },
+        {
+            name: "Mario4",
+            lastname: "Rossi4",
+            email: "mariorossi4@email.it",
+            fiscalcode: "B192393FF83493",
+            province: "PD",
+            phone: "13939404"
+        },
+    ];
+
     getUsers() {
-        return  [
-            {
-                name: "Mario",
-                lastname: "Rossi",
-                email: "mariorossi@email.it",
-                fiscalcode: "F192393FF83493",
-                province: "PD",
-                phone: "13939404"
-            },
-            {
-                name: "Mario2",
-                lastname: "Rossi2",
-                email: "mariorossi2@email.it",
-                fiscalcode: "C192393FF83493",
-                province: "PD",
-                phone: "13939404"
-            },
-            {
-                name: "Mario3",
-                lastname: "Rossi3",
-                email: "mariorossi3@email.it",
-                fiscalcode: "A192393FF83493",
-                province: "VE",
-                phone: "13939404"
-            },
-            {
-                name: "Mario4",
-                lastname: "Rossi4",
-                email: "mariorossi4@email.it",
-                fiscalcode: "B192393FF83493",
-                province: "PD",
-                phone: "13939404"
-            },
-        ]
+        return this.users;
+    }
+
+    deleteUsers(user) {
+        let index = this.users.indexOf(user);
+        if(index >= 0){
+            this.users.splice(index, 1);
+        }
     }
 }
